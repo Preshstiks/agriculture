ef6a730b415c62033a765336f12161d744ea4249
Davies Precious <preshy.d5@gmail.com>
2023-11-24 00:01:42 +0100

    app edited

--- pages/gallery.js
+++ pages/gallery.js
@@ -87,7 +87,7 @@ const Gallery = () => {
           ))}
         </div>
       ) : (
-        <div className="gallery">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {gallery.map((item, index) => (
             <motion.div
               key={index}
